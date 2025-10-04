111ac15fc2b86e1cb6e872fed60d4d1b73c039e0
Nate233-cmd <nateaboagye@gmail.com>
2025-10-04 11:55:34 +0000

    Create library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,10 @@
+Available books after setup:
+Brave New World by Aldous Huxley
+1984 by George Orwell
+
+Available books after checking out '1984':
+Brave New World by Aldous Huxley
+
+Available books after returning '1984':
+Brave New World by Aldous Huxley
+1984 by George Orwell
